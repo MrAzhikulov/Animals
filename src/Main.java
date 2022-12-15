8c2908b8c5ca1bc9af0ad455686afb6c576d33e4
sterben <mr.azhikulov@gmail.com>
2022-12-15 19:51:58 +0600

    up

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner console = new Scanner(System.in);
+        System.out.println("1-Dog"+"\n2-Cat"+"\n3-Horse");
+        int countdog = console.nextInt();
     }
 }
